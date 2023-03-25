3773df077d23751fb64e3571c6948f601b18fbe5
Ruslan-Far <rfarhetdinov@mail.ru>
2023-03-25 19:06:37 +0300

    реализовал нахождение определителя

--- slau.cu
+++ slau.cu
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-#define N 4
+#define N 2
 #define SIZE N * N
 
 __device__ float fact(int n)
@@ -15,43 +15,81 @@ __device__ float fact(int n)
 	return factorial;
 }
 
-__global__ void searchDet(int *a, int *det)
+__device__ void print_matrix(double *a)
 {
-	int term;
-	int n_fact;
-
-	n_fact = fact(N);
-	printf("n_fact = %d\n", n_fact);
-	*det = 0;
-	for (int k = 0; k < n_fact; k++)
+	for (int i = 0; i < N; i++)
 	{
-		term = pow(-1, k);
-		if (term > 0)
+		for (int j = 0; j < N; j++)
 		{
-			for (int i = 0, cpk = k; i < N; i++)
-			{
-				printf("%d\n", a[N * i + cpk % N]);
-				term *= a[N * i + cpk % N];
-				cpk++;
-			}
+			printf("%f ", a[N * i + j]);
 		}
-		else
-		{
-			for (int i = 0, cpk = k; i < N; i++)
-			{
-				printf("%d\n", a[N * i + cpk % N]);
-				term *= a[N * i + cpk % N];
-				cpk--;
-				if (cpk < 0)
-					cpk = N - 1;
-			}
-		}
-		printf("term = %d\n\n\n", term);
-		*det += term;
+		printf("\n");
 	}
 }
 
-void checkCUDAError(const char *msg)
+__device__ void transform_matrix(double *a)
+{
+	double divider;
+
+	for (int k = 0; k < N - 1; k++)
+	{
+		for (int i = k; i < N - 1; i++)
+		{
+			divider = a[N * (i + 1) + k] / a[N * k + k];
+			// printf("divider = %f\n", divider);
+			for (int j = 0; j < N; j++)
+			{
+				a[N * (i + 1) + k + j] -= divider * a[N * k + k + j];
+			}
+		}
+	}
+}
+
+__global__ void search_det(double *a, double *det)
+{
+	transform_matrix(a);
+	print_matrix(a);
+	*det = 1;
+	for (int i = 0; i < N; i++)
+	{
+		*det *= a[N * i + i];
+	}
+	printf("%f\n", *det);
+	// int term;
+	// int n_fact;
+
+	// n_fact = fact(N);
+	// printf("n_fact = %d\n", n_fact);
+	// *det = 0;
+	// for (int k = 0; k < n_fact; k++)
+	// {
+	// 	term = pow(-1, k);
+	// 	if (term > 0)
+	// 	{
+	// 		for (int i = 0, cpk = k; i < N; i++)
+	// 		{
+	// 			printf("%d\n", a[N * i + cpk % N]);
+	// 			term *= a[N * i + cpk % N];
+	// 			cpk++;
+	// 		}
+	// 	}
+	// 	else
+	// 	{
+	// 		for (int i = 0, cpk = k; i < N; i++)
+	// 		{
+	// 			printf("%d\n", a[N * i + cpk % N]);
+	// 			term *= a[N * i + cpk % N];
+	// 			cpk--;
+	// 			if (cpk < 0)
+	// 				cpk = N - 1;
+	// 		}
+	// 	}
+	// 	printf("term = %d\n\n\n", term);
+	// 	*det += term;
+	// }
+}
+
+void check_cuda_error(const char *msg)
 {
     cudaError_t err = cudaGetLastError();
 
@@ -64,26 +102,29 @@ void checkCUDAError(const char *msg)
 
 int	main()
 {
-	// int host_a[SIZE] = {2, 3, 4, 1};
-	// int host_a[SIZE] = {1, -2, 3, 4, 90, 6, -7, 8, 9};
-	int host_a[SIZE] = {5, 3, 21, 7, 4, 47, 12, 18, 77, 45, 3, 1, -6, 90, 34, -82};
+	double  host_a[SIZE] = {2, 3, 4, 1};
+	// double  host_a[SIZE] = {1, -2, 3, 4, 90, 6, -7, 8, 9};
+	// double host_a[SIZE] = {5, 3, 21, 7, 4, 47, 12, 18, 77, 45, 3, 1, -6, 90, 34, -82};
+	// double host_a[SIZE] = {5, 3, 21, 7, 4, 47, 12, 18, 77, 45, 3, 1, -6, 90, 34, -82, -103, 71, 51, 21, 33, -367, 16, 2, 1};
 	// int host_b[N] = {8, 6};
-	int host_det;
-	int *dev_a;
-	int *dev_det;
-	int	size;
+	double host_det;
+	double *dev_a;
+	double *dev_det;
+	int	int_size;
+	int double_size;
 
-	size = sizeof(int);
-	host_det = 0;
-	cudaMalloc(&dev_a, size * SIZE);
-	cudaMalloc(&dev_det, size);
-	cudaMemcpy(dev_a, host_a, size * SIZE, cudaMemcpyHostToDevice);
-	searchDet<<<1, 1>>>(dev_a, dev_det);
-	cudaMemcpy(&host_det, dev_det, size, cudaMemcpyDeviceToHost);
-	printf("Определитель матрицы А = %d\n", host_det);
+	int_size = sizeof(int);
+	double_size = sizeof(double);
+	cudaMalloc(&dev_a, double_size * SIZE);
+	cudaMalloc(&dev_det, double_size);
+	cudaMemcpy(dev_a, host_a, double_size * SIZE, cudaMemcpyHostToDevice);
+	search_det<<<1, 1>>>(dev_a, dev_det);
+	cudaMemcpy(&host_det, dev_det, double_size, cudaMemcpyDeviceToHost);
+	host_det = round(host_det);
+	printf("Определитель матрицы А = %f\n", host_det);
 	cudaFree(dev_a);
 	cudaFree(dev_det);
 
-	checkCUDAError("");
+	check_cuda_error("");
 	return 0;
 }
