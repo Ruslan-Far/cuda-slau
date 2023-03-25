34622a8a078ef40607aefe3addd484b8f0c546e7
Ruslan-Far <rfarhetdinov@mail.ru>
2023-03-25 23:45:28 +0300

    в процессе добавления миноров

--- slau.cu
+++ slau.cu
@@ -3,52 +3,90 @@
 #define N 2
 #define SIZE N * N
 
-__device__ void print_matrix(double *a)
+__device__ void print_matrix(double *a, int n)
 {
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < N; j++)
+		for (int j = 0; j < n; j++)
 		{
-			printf("%f ", a[N * i + j]);
+			printf("%f ", a[n * i + j]);
 		}
 		printf("\n");
 	}
 }
 
-__device__ void transform_matrix(double *a)
+__device__ void transform_matrix(double *a, int n)
 {
 	double divider;
 
-	for (int k = 0; k < N - 1; k++)
+	for (int k = 0; k < n - 1; k++)
 	{
-		for (int i = k; i < N - 1; i++)
+		for (int i = k; i < n - 1; i++)
 		{
-			divider = a[N * (i + 1) + k] / a[N * k + k];
-			for (int j = 0; j < N; j++)
+			divider = a[n * (i + 1) + k] / a[n * k + k];
+			for (int j = 0; j < n; j++)
 			{
-				a[N * (i + 1) + k + j] -= divider * a[N * k + k + j];
+				a[n * (i + 1) + k + j] -= divider * a[n * k + k + j];
 			}
 		}
 	}
 }
 
-__device__ int get_det(double *a)
+__device__ int get_det(double *a, int n)
 {
 	double det;
 
-	transform_matrix(a);
-	print_matrix(a);
+	if (n == 0)
+		n = N;
+	transform_matrix(a, n);
+	print_matrix(a, n);
 	det = 1;
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < n; i++)
 	{
-		det *= a[N * i + i];
+		det *= a[n * i + i];
 	}
 	return ((int) round(det));
 }
 
-__global__ void search_det_and_minor_matrix(double *a, int *det)
+__device__ int get_n()
 {
-	*det = get_det(a);
+	int k;
+
+	k = 1;
+	for (int i = 1; i < SIZE; i += k)
+	{
+		k += 2;
+	}
+	printf("__fsqrt_rn(SIZE - k) = %d\n", (int) __fsqrt_rn(SIZE - k));
+	return __fsqrt_rn(SIZE - k);
+}
+
+__device__ void init_sub_a(double *a, double *sub_a, int i, int j)
+{
+
+}
+
+__global__ void search_det(double *a, int *det)
+{
+	*det = get_det(a, 0);
+}
+
+__global__ void search_minor_matrix(double *a, int *minor)
+{
+	double *sub_a;
+	int n;
+
+	n = get_n();
+	cudaMalloc(&sub_a, sizeof(double) * SIZE);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			init_sub_a(a, sub_a, i, j);
+			minor[N * i + j] = get_det(sub_a, n);
+		}
+	}
+	cudaFree(sub_a);
 }
 
 void check_cuda_error(const char *msg)
@@ -69,20 +107,31 @@ int	main()
 	// double host_a[SIZE] = {5, 3, 21, 7, 4, 47, 12, 18, 77, 45, 3, 1, -6, 90, 34, -82};
 	// double host_a[SIZE] = {5, 3, 21, 7, 4, 47, 12, 18, 77, 45, 3, 1, -6, 90, 34, -82, -103, 71, 51, 21, 33, -367, 16, 2, 1};
 	// int host_b[N] = {8, 6};
+	int *host_minor;
 	int host_det;
 	double *dev_a;
+	double *dev_sub_a;
 	int *dev_det;
+	int *dev_minor;
 	int	int_size;
 	int double_size;
 
 	int_size = sizeof(int);
 	double_size = sizeof(double);
+	host_minor = (int *) malloc(int_size * SIZE);
 	cudaMalloc(&dev_a, double_size * SIZE);
+	cudaMalloc(&dev_sub_a, double_size * SIZE);
 	cudaMalloc(&dev_det, int_size);
+	cudaMalloc(&dev_minor, int_size * SIZE);
+
 	cudaMemcpy(dev_a, host_a, double_size * SIZE, cudaMemcpyHostToDevice);
-	search_det_and_minor_matrix<<<1, 1>>>(dev_a, dev_det);
+	search_det<<<1, 1>>>(dev_a, dev_det);
 	cudaMemcpy(&host_det, dev_det, int_size, cudaMemcpyDeviceToHost);
 	printf("Определитель матрицы А = %d\n", host_det);
+	// cudaMemcpy(dev_a, host_a, double_size * SIZE, cudaMemcpyHostToDevice);
+	// search_minor_matrix<<<1, 1>>>(dev_a, dev_minor);
+
+	free(host_minor);
 	cudaFree(dev_a);
 	cudaFree(dev_det);
 
